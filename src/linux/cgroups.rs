e99e0b3a2f4c9a2f9e788eb34a44d2f3a16d58b9
Ivanq <imachug@yandex.ru>
2022-12-07 18:09:04 +0300

    Use fchownat instead of fchown

--- src/linux/cgroups.rs
+++ src/linux/cgroups.rs
@@ -99,14 +99,12 @@ impl Cgroup {
             .write(b"+cpu +memory +pids\n")
             .context("Failed to enable cgroup controllers")?;
 
-        let proc_cgroup = self
-            .core_cgroup_fd
-            .sub_dir(format!("proc-{id}"))
-            .with_context(|| format!("Failed to open proc-{id}"))?;
-        nix::unistd::fchown(
-            proc_cgroup.as_raw_fd(),
+        nix::unistd::fchownat::<str>(
+            Some(self.core_cgroup_fd.as_raw_fd()),
+            &format!("proc-{id}"),
             Some(nix::unistd::Uid::from_raw(ids::EXTERNAL_ROOT_UID)),
             Some(nix::unistd::Gid::from_raw(ids::EXTERNAL_ROOT_GID)),
+            nix::unistd::FchownatFlags::NoFollowSymlink,
         )
         .context("Failed to chown <cgroup>")?;
 
