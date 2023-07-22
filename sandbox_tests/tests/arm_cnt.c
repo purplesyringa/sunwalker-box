75d0d40ccac440816a284b0f03ebb114091d65e7
Yuki Sireneva <yuki.utk8g@gmail.com>
2023-07-22 21:10:06 +0300

    tests/arm_cnt -- combine all counter-timer registers in one test

---
+++ sandbox_tests/tests/arm_cnt.c
@@ -0,0 +1,22 @@
+/*
+description: arm64 counter-timer registers (cntfrq, cntpct[ss], cntvct[ss]) are unavailable
+arch:
+- aarch64
+expect:
+  limit_verdict: Signaled
+  exit_code: -4
+  runs: 4
+  pass_run_number: true
+*/
+
+int main(int argc, char **argv) {
+  int run = argv[1][0] - '0';
+  long long val;
+  if (run == 0) asm volatile("mrs %0, cntfrq_el0" : "=r" (val));
+  if (run == 1) asm volatile("mrs %0, cntpctss_el0" : "=r" (val));
+  if (run == 2) asm volatile("mrs %0, cntpct_el0" : "=r" (val));
+  if (run == 3) asm volatile("mrs %0, cntvctss_el0" : "=r" (val));
+  if (run == 4) asm volatile("mrs %0, cntvct_el0" : "=r" (val));
+  printf("Counter is %lli\n", val);
+  return 0;
+}
