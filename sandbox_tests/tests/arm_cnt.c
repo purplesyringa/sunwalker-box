b6551e2e3b924b9105f27a843b5c3bfc54946ffc
Alisa Sireneva <imachug@yandex.ru>
2023-07-26 19:21:08 +0300

    Update arm_cnt test

--- sandbox_tests/tests/arm_cnt.c
+++ sandbox_tests/tests/arm_cnt.c
@@ -13,16 +13,40 @@ pass_run_number: true
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-  int run = atoi(argv[1]);
-  long long val;
-  const char* name;
-#define REG(reg_name) if(run-- == 0) { name = #reg_name; asm volatile("mrs %0, " #reg_name "_el0" : "=r" (val) ); }
+unsigned long long load_register(int reg) {
+  unsigned long long val;
+
+#define REG(reg_name) \
+  if(reg-- == 0) { \
+    asm volatile("mrs %0, " #reg_name "_el0" : "=r"(val)); \
+  }
   REG(cntpctss)
   REG(cntpct)
   REG(cntvctss)
   REG(cntvct)
-  printf("counter %s is %lli -- should be unavailable or random\n", name, val);
+#undef REG
+
+  return val;
+}
+
+const char* names[] = {"cntpctss", "cntpct", "cntvctss", "cntvct"};
+
+int main(int argc, char **argv) {
+  int run = atoi(argv[1]);
+
+  unsigned long long before = load_register(run);
+  if (usleep(300000) == -1) {
+    perror("usleep");
+    return 1;
+  }
+  unsigned long long after = load_register(run);
+
+  if (before < (after - before) / 10) {
+    asm volatile("udf #0");
+  }
+
+  printf("Counter %s had values %llu and %llu, indicating uptime of more than 30ms\n", names[run], before, after);
   return 0;
 }
