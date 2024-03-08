2f29165f67434a1bd4278ba348a1b96205cf974c
Alisa Sireneva <me@purplesyringa.moe>
2024-03-08 14:47:02 +0300

    cpu_time_limit: Increase tolerance
    Apparently 0.01 is not enough for my aarch64 machine

--- sandbox_tests/tests/cpu_time_limit.c
+++ sandbox_tests/tests/cpu_time_limit.c
@@ -2,10 +2,10 @@
 description: CPU time limit works
 script: |
   expect(
-    run(cpu_time_limit=0.2),
+    run(cpu_time_limit=0.5),
     limit_verdict="CPUTimeLimitExceeded",
-    real_time="0.2 +- 0.05",
-    cpu_time="0.2 +- 0.01",
+    real_time="0.5 +- 0.05",
+    cpu_time="0.5 +- 0.03",
     idleness_time="0 +- 0.05"
   )
 */
