1c130dfea47f92dcf73c822a1853c70536a8368b
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-03-08 19:59:18 +0300

    Add arm64 syscall wrappers
    To ensure that C++ code is portable between amd64 and arm64

--- cxx/libc.hpp
+++ cxx/libc.hpp
@@ -50,6 +50,67 @@ long raw_syscall(long number, long arg1, long arg2, long arg3, long arg4, long a
                  : "rcx", "r11", "memory");
     return number;
 }
+#elif defined(__aarch64__)
+long raw_syscall(long number) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0");
+    asm volatile("svc 0" : "=r"(x0) : "r"(w8) : "memory");
+    return x0;
+}
+long raw_syscall(long number, long arg0) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0") = arg0;
+    asm volatile("svc 0" : "+r"(x0) : "r"(w8) : "memory");
+    return x0;
+}
+long raw_syscall(long number, long arg0, long arg1) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0") = arg0;
+    register long x1 asm("x1") = arg1;
+    asm volatile("svc 0" : "+r"(x0) : "r"(w8), "r"(x1) : "memory");
+    return x0;
+}
+long raw_syscall(long number, long arg0, long arg1, long arg2) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0") = arg0;
+    register long x1 asm("x1") = arg1;
+    register long x2 asm("x2") = arg2;
+    asm volatile("svc 0" : "+r"(x0) : "r"(w8), "r"(x1), "r"(x2) : "memory");
+    return x0;
+}
+long raw_syscall(long number, long arg0, long arg1, long arg2, long arg3) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0") = arg0;
+    register long x1 asm("x1") = arg1;
+    register long x2 asm("x2") = arg2;
+    register long x3 asm("x3") = arg3;
+    asm volatile("svc 0" : "+r"(x0) : "r"(w8), "r"(x1), "r"(x2), "r"(x3) : "memory");
+    return x0;
+}
+long raw_syscall(long number, long arg0, long arg1, long arg2, long arg3, long arg4) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0") = arg0;
+    register long x1 asm("x1") = arg1;
+    register long x2 asm("x2") = arg2;
+    register long x3 asm("x3") = arg3;
+    register long x4 asm("x4") = arg4;
+    asm volatile("svc 0" : "+r"(x0) : "r"(w8), "r"(x1), "r"(x2), "r"(x3), "r"(x4) : "memory");
+    return x0;
+}
+long raw_syscall(long number, long arg0, long arg1, long arg2, long arg3, long arg4, long arg5) {
+    register long w8 asm("x8") = number;
+    register long x0 asm("x0") = arg0;
+    register long x1 asm("x1") = arg1;
+    register long x2 asm("x2") = arg2;
+    register long x3 asm("x3") = arg3;
+    register long x4 asm("x4") = arg4;
+    register long x5 asm("x5") = arg5;
+    asm volatile("svc 0"
+                 : "+r"(x0)
+                 : "r"(w8), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5)
+                 : "memory");
+    return x0;
+}
 #else
 #error Trying to compile syscall wrappers against unsupported architecture!
 #endif
