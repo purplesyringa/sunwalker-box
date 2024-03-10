26442a70e463f0cd302891ece3dda798ad42ca6f
Alisa Sireneva <me@purplesyringa.moe>
2024-03-10 08:35:21 +0300

    Fix race in cow_stats test
    The child could exit and drop mm before the parent even continued
    execution after fork, leading to test failure.

--- sandbox_tests/tests/cow_stats.c
+++ sandbox_tests/tests/cow_stats.c
@@ -5,11 +5,9 @@ script: |
 */
 
 #include <errno.h>
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/socket.h>
 #include <sys/mman.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
@@ -17,11 +15,17 @@ int main() {
                  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (p == MAP_FAILED) {
     perror("mmap");
-    exit(1);
+    return 1;
   }
 
   for (size_t i = 4 * 1024 * 1024; i < 10 * 1024 * 1024; i += 4096) {
     p[i] = '\0';
+  }
+
+  int sv[2];
+  if (socketpair(AF_UNIX, SOCK_DGRAM, 0, sv) == -1) {
+    perror("socketpair");
+    return 1;
   }
 
   pid_t child = fork();
@@ -31,18 +35,21 @@ int main() {
   }
 
   // Rewrite just 4 MiB; this is divisible by 2 MiB, which is huge page size on x86-64
-  if (child == 0) {
-    for (size_t i = 0; i < 4 * 1024 * 1024; i += 4096) {
-      p[i] = '\1';
-    }
-  } else {
-    for (size_t i = 0; i < 4 * 1024 * 1024; i += 4096) {
-      p[i] = '\2';
-    }
-    int wstatus;
-    if (wait(&wstatus) != child) {
-      perror("wait");
-      return 1;
-    }
+  int index = child == 0;
+  for (size_t i = 0; i < 4 * 1024 * 1024; i += 4096) {
+    p[i] = 1 + index;
   }
+
+  if (write(sv[index], ".", 1) == -1) {
+    perror("write");
+    return 1;
+  }
+
+  char c;
+  if (read(sv[index], &c, 1) != 1) {
+    perror("read");
+    return 1;
+  }
+
+  return 0;
 }
