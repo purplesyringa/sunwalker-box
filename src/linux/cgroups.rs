843ac46fd68fc53f60606e0fce788441c44fcbbc
Alisa Sireneva <me@purplesyringa.moe>
2024-03-10 22:31:44 +0300

    Prevent failure in parasite in almost-OOM conditions

--- src/linux/cgroups.rs
+++ src/linux/cgroups.rs
@@ -275,6 +275,16 @@ impl BoxCgroup {
         buf.trim().parse().context("Invalid memory.peak format")
     }
 
+    pub fn get_memory_use(&self) -> Result<usize> {
+        let mut buf = String::new();
+        self.proc_cgroup_fd
+            .open_file(format!("box-{}/memory.current", self.box_id))
+            .context("Failed to open memory.current for reading")?
+            .read_to_string(&mut buf)
+            .context("Failed to read memory.current")?;
+        buf.trim().parse().context("Invalid memory.current format")
+    }
+
     pub fn get_memory_stats(&self) -> Result<MemoryStats> {
         let mut buf = String::new();
         self.proc_cgroup_fd
