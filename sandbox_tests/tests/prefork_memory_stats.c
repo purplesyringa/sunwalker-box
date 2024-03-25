0e57cbd00b28fae358012ceb66ab3fe69b19a1ae
Alisa Sireneva <me@purplesyringa.moe>
2024-03-26 00:36:32 +0300

    Make memory tests in prefork more thorough

--- sandbox_tests/tests/prefork_memory_stats.c
+++ sandbox_tests/tests/prefork_memory_stats.c
@@ -1,13 +1,21 @@
 /*
 description: Peak memory usage is computed correctly in prefork
 script: |
-    pid = prefork(Run(argv + ["5000000", "10000000"]))
+    pid = prefork(Run(argv + ["5000000", "1", "10000000"]))
     expect(pid, verdict=Suspended)
     expect(resume(pid), metrics=ApproximateMetrics(memory="10 MB +- 0.5 MB"))
 
-    pid = prefork(Run(argv + ["10000000", "5000000"]))
+    pid = prefork(Run(argv + ["10000000", "1", "5000000"]))
     expect(pid, verdict=Suspended)
     expect(resume(pid), metrics=ApproximateMetrics(memory="10 MB +- 0.5 MB"))
+
+    pid = prefork(Run(argv + ["5000000", "0", "10000000"]))
+    expect(pid, verdict=Suspended)
+    expect(resume(pid), metrics=ApproximateMetrics(memory="15 MB +- 0.5 MB"))
+
+    pid = prefork(Run(argv + ["10000000", "0", "5000000"]))
+    expect(pid, verdict=Suspended)
+    expect(resume(pid), metrics=ApproximateMetrics(memory="15 MB +- 0.5 MB"))
 */
 
 #include <errno.h>
@@ -16,7 +24,7 @@ script: |
 #include <stdlib.h>
 #include <sys/mman.h>
 
-void commit(size_t size) {
+void commit(size_t size, int unmap) {
     void *p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (p == MAP_FAILED) {
         perror("mmap");
@@ -25,12 +33,14 @@ void commit(size_t size) {
     for (size_t i = 0; i < size; i += 4096) {
         *((char *)p + i) = '\0';
     }
-    munmap(p, size);
+    if (unmap) {
+        munmap(p, size);
+    }
 }
 
 int main(int argc, char **argv) {
-    commit(atoi(argv[1]));
+    commit(atoi(argv[1]), atoi(argv[2]));
     puts("Suspend here");
     fflush(stdout);
-    commit(atoi(argv[2]));
+    commit(atoi(argv[3]), 0);
 }
