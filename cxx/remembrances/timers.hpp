6dc6b95f8e04f71d295cdb134a035bda540d0ecc
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-03-27 07:40:26 +0300

    Use sigevent instead of custom Timer structure for C++ code

--- cxx/remembrances/timers.hpp
+++ cxx/remembrances/timers.hpp
@@ -1,6 +1,7 @@
 #include "../libc.hpp"
 #include "../result.hpp"
 #include <array>
+#include <asm-generic/siginfo.h>
 
 namespace timers {
 
@@ -31,17 +32,10 @@ Result<void> save(ParasiteState &state) {
     return {};
 }
 
-struct Timer {
-    int id;
-    int signal;
-    void *sigev_value;
-    int mechanism;
-    pid_t target;
-    int clock_id;
-};
-
 struct State {
-    std::array<Timer, 64> timers;
+    std::array<int, 64> indices;
+    std::array<int, 64> clock_ids;
+    std::array<sigevent, 64> timers;
     std::array<itimerspec, 64> intervals;
     size_t count;
 };
@@ -53,13 +47,8 @@ struct State {
 #define sigev_notify_thread_id _sigev_un._tid
 #endif
 
-Result<void> add_timer(const Timer &timer) {
-    static int timer_id;
-    static sigevent sev;
-    sev.sigev_value.sival_ptr = timer.sigev_value;
-    sev.sigev_signo = timer.signal;
-    sev.sigev_notify = timer.mechanism;
-    sev.sigev_notify_thread_id = timer.target;
+Result<void> add_timer(const int timer_id, const int clock_id, const sigevent &sev) {
+    static int actual_timer_id;
 
     // timer_create(3p) in libc says that SIGEV_THREAD creates new thread for signal handler, but
     // proc(5) for `/proc/[pid]/timers` has zero information on what the function for SIGEV_THREAD
@@ -70,41 +59,38 @@ Result<void> add_timer(const Timer &timer) {
     // And how am I supposed to know this without summoning a Satan? Not saying this is a bad thing,
     // just... I was not prepared for it.
 
-    libc::timer_create(timer.clock_id, &sev, &timer_id).CONTEXT("Could not create timer").TRY();
-    ENSURE(timer.id == timer_id, "Unexpected timer ID");
+    libc::timer_create(clock_id, const_cast<sigevent *>(&sev), &actual_timer_id)
+        .CONTEXT("Could not create timer")
+        .TRY();
+    ENSURE(timer_id == actual_timer_id, "Unexpected timer ID");
     return {};
 }
 
 Result<void> add_dummy_timer(const int next_timer_id) {
-    Timer dummy = {next_timer_id, 0, 0, SIGEV_NONE, 0, CLOCK_REALTIME};
-    return add_timer(dummy).CONTEXT("Could not add dummy timer");
+    static sigevent dummy{.sigev_notify = SIGEV_NONE};
+    return add_timer(next_timer_id, CLOCK_REALTIME, dummy).CONTEXT("Could not add dummy timer");
 }
 
 Result<void> load(const State &state) {
-    static int next_timer_id = 0;
+    static int next_timer_id;
+    next_timer_id = 0;
 
     ENSURE(state.count <= 64, "Too many timers!");
 
     for (int i = 0; i < state.count; ++i) {
-        while (next_timer_id < state.timers[i].id) {
+        while (next_timer_id < state.indices[i]) {
             // Create a temporary unused timer to fill the void so that our timer gets the right ID
             add_dummy_timer(next_timer_id).CONTEXT("While dummy creating").TRY();
+            libc::timer_delete(next_timer_id).CONTEXT("While dummy destroying").TRY();
             next_timer_id += 1;
         }
-        add_timer(state.timers[i]).CONTEXT("While timer creating").TRY();
-        libc::timer_settime(state.timers[i].id, 0, &state.intervals[i], nullptr)
-            .CONTEXT("Could not arm timer")
-            .TRY();
-        next_timer_id += 1;
-    }
 
-    // Remove temporary timers
-    next_timer_id = 0;
-    for (int i = 0; i < state.count; ++i) {
-        while (next_timer_id < state.timers[i].id) {
-            libc::timer_delete(next_timer_id).CONTEXT("Could not delete dummy timer").TRY();
-            next_timer_id += 1;
-        }
+        add_timer(state.indices[i], state.clock_ids[i], state.timers[i])
+            .CONTEXT("While timer creating")
+            .TRY();
+        libc::timer_settime(state.indices[i], 0, &state.intervals[i], nullptr)
+            .CONTEXT("While arming timer")
+            .TRY();
         next_timer_id += 1;
     }
 
