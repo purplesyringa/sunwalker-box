6bee3f48fcdeb86edd9d979c168784840cd71f77
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-03-27 03:09:45 +0300

    What if MAP_NORESERVE

--- sandbox_tests/tests/cow_stats.c
+++ sandbox_tests/tests/cow_stats.c
@@ -11,8 +11,8 @@ script: |
 #include <unistd.h>
 
 int main() {
-    char *p =
-        mmap(NULL, 10 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    char *p = mmap(NULL, 10 * 1024 * 1024, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
     if (p == MAP_FAILED) {
         perror("mmap");
         return 1;
