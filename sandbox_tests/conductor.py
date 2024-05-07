bd7ae35bbda50002a207d56fc825ad8882bc6e4a
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-05-07 11:45:45 +0300

    sandbox_tests: Add compile and prepare progress
    This effectively drops `make` dependency for sandbox_tests

---
+++ sandbox_tests/conductor.py
@@ -0,0 +1,197 @@
+from concurrent.futures import ThreadPoolExecutor
+import dataclasses
+import time
+import threading
+import traceback
+import queue
+from typing import Optional
+
+
+@dataclasses.dataclass
+class EventStarted:
+    slugslug: str
+    slug: str
+
+
+@dataclasses.dataclass
+class EventFinished:
+    slugslug: str
+    slug: str
+    description: str
+    ex: Optional[Exception] = None
+    trace: Optional[str] = None
+    duration: Optional[float] = None
+    error: Optional[queue.Queue] = None
+
+    def __post_init__(self):
+        if not self.ex:
+            self.kind = "pass"
+        elif isinstance(self.ex, AssertionError):
+            self.kind = "failure"
+        else:
+            self.kind = "crash"
+
+    def as_strings(self):
+        time_text = f"{self.duration:.1f}s" if (self.duration or 0) > 0.1 else "    "
+        yield f"\x1b[36m{time_text}\x1b[0m "
+
+        status = "\x1b[32m OK \x1b[0m" if self.kind == "pass" else "\x1b[91mFAIL\x1b[0m"
+        yield f'{status} {self.slug}'
+
+        if self.kind != "pass":
+            yield '\n'
+            yield f"\x1b[36m  {self.description}\x1b[0m\n"
+
+            while not self.error.empty():
+                message = self.error.get_nowait()
+                yield from ('  --- executor message ---\n    ', message.replace('\n', '\n    '), '\n')
+
+            if self.kind == "failure":
+                yield from ("\x1b[33m  ", str(self.ex).replace("\n", "\n  "), "\x1b[0m\n")
+                try:
+                    yield from ('    ', '\n    '.join(self.ex.__notes__))
+                except AttributeError:
+                    pass
+            elif self.kind == "crash":
+                yield from ("\x1b[95m", self.ex, "\x1b[0m\n", self.trace)
+
+
+class Conductor:
+    def __init__(self, name, action, end_action, actions, current_status=True, intermediate_status=False, hide_passed=True, bail_on_fail=True):
+        self.name = name
+        self.action = action
+        self.end_action = end_action
+        self.actions = actions
+
+        self.current_status = current_status
+        self.intermediate_status = intermediate_status
+        self.hide_passed = hide_passed
+        self.bail_on_fail = bail_on_fail
+
+        self.count = 0
+        self.passes = 0
+        self.failures = 0
+        self.crashes = 0
+        self.current = set()
+        self.feedback = queue.Queue()
+
+    def is_running(self):
+        return self.count != len(self.actions)
+
+    def as_strings(self):
+        _italic = "\x1b[3m"
+        _reset = "\x1b[0m"
+        running = self.is_running()
+
+        yield '\r\x1b[K'
+
+        action = self.action if running else self.end_action
+        yield f"{action} [{self.count}/{len(self.actions)}]"
+
+        for pattern, cnt in [
+            ("\x1b[32m{} passed", self.passes),
+            ("\x1b[91m{} failed", self.failures),
+            ("\x1b[95m{} crashed", self.crashes),
+            ("\x1b[36m{} not executed", len(self.actions) - self.count),
+        ]:
+            if cnt > 0:
+                yield from ('  ', pattern.format(cnt), _reset)
+
+        if running:
+            yield ' -- '
+            yield ", ".join(map(lambda item: f"\x1b[3m{item}{_reset}", self.current))
+            yield '...'
+
+    def print_status(self):
+        print(*self.as_strings(), flush=True, sep='', end='')
+
+    def _started(self, event):
+        self.current.add(event.slugslug)
+        return True
+
+    def _finished(self, event):
+        self.current.remove(event.slugslug)
+        self.count += 1
+
+        if event.kind == "pass":
+            self.passes += 1
+        elif event.kind == "failure":
+            self.failures += 1
+        elif event.kind == "crash":
+            self.crashes += 1
+
+        if not self.hide_passed or event.kind != 'pass':
+            print(f'\r\x1b[K', *event.as_strings(), sep='', flush=True)
+
+        return event.kind == "pass" or not self.bail_on_fail
+
+    def _unknown(self, event):
+        self.crashes += 1
+        print(f'\r\x1b[K{event}', flush=True)
+        return not self.bail_on_fail
+
+    def _dispatch_event(self):
+        if self.current_status:
+            self.print_status()
+
+        event = self.feedback.get()
+
+        if type(event) is EventStarted:
+            return self._started(event)
+        elif type(event) is EventFinished:
+            return self._finished(event)
+
+        return self._unknown(event)
+
+    def _run_single(self, cpus, action, args, kwargs):
+        try:
+            self.feedback.put(EventStarted(action.slugslug, action.slug))
+
+            thread_name = threading.current_thread().name
+            cpu_index = int(thread_name.rsplit('_', 1)[1])
+            cpu = cpus[cpu_index]
+
+            error = queue.Queue()
+
+            start_time = time.time()
+            try:
+                res = action.run(cpu, error, *args, **kwargs)
+            except Exception as e:
+                ex, trace = e, traceback.format_exc()
+            else:
+                ex, trace = None, None
+            end_time = time.time()
+
+            duration = end_time - start_time
+
+        except Exception as e:
+            ex, trace = e, traceback.format_exc()
+
+        try:
+            self.feedback.put(EventFinished(
+                action.slugslug,
+                action.slug,
+                action.description,
+                **{key: locals().get(key) for key in "ex trace duration error".split()}
+            ))
+        except Exception as e:
+            print(e)
+
+    def run(self, cpus, /, *args, **kwargs):
+        executor = ThreadPoolExecutor(max_workers=len(cpus), thread_name_prefix=self.name)
+        for action in self.actions:
+            executor.submit(self._run_single, cpus, action, args, kwargs)
+
+        try:
+            while self.is_running() and self._dispatch_event():
+                continue
+        except KeyboardInterrupt:
+            print()
+            return False
+
+        executor.shutdown(wait=False, cancel_futures=True)
+        self.print_status()
+        if self.intermediate_status:
+            print()
+
+        return self.count == self.passes
--- sandbox_tests/test.py
+++ sandbox_tests/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-from concurrent.futures import ThreadPoolExecutor
+import conductor
 import dataclasses
 import io
 import json
@@ -9,9 +9,6 @@ import re
 import shutil
 import subprocess
 import sys
-import time
-import threading
-import traceback
 import queue
 from typing import Optional
 import yaml
@@ -32,6 +29,7 @@ class Test:
         self.assets_dir = None
         self.files_committed = False
         self.argv = []
+        self.preparable = self.root is not None or self.assets is not None
 
     def _create_dirs(self, structure: dict[str, ...], dir: str):
         for name, value in structure.items():
@@ -50,7 +48,7 @@ class Test:
 
             os.lchown(path, 2, 2)  # internal user
 
-    def prepare(self, tester):
+    def prepare(self):
         if self.root is not None:
             self.root_dir = f"build/roots/{self.slug}"
             if os.path.isdir(self.root_dir):
@@ -74,32 +72,54 @@ class Test:
     def extract_header(path):
         ...
 
+    def build(self, error: queue.Queue):
+        ...
+
+    def needs_build(self):
+        return False
+
 
 class CTest(Test):
-    def prepare(self, tester):
-        ccflags = " -static" if self.static else ""
+    buildable = True
 
+    def __post_init__(self):
+        self._source = f'tests/{self.slug}.c'
+        self._binary = f'build/{self.slug}'
+
+        super().__post_init__()
+
+    def _test_compiler(self, name):
         try:
-            _gcc = subprocess.run(["gcc", "-v"], capture_output=True, text=True).stderr
-        except Exception:
-            _gcc = ""
+            return 'musl' in subprocess.run([name, '-v'], capture_output=True, text=True).stderr
+        except Exception as e:
+            return False
 
-        try:
-            _musl_gcc = subprocess.run(["musl-gcc", "-v"], capture_output=True, text=True).stderr
-        except Exception:
-            _musl_gcc = ""
+    def build(self, error):
+        ccflags = ["-static"] if self.static else []
 
-        if "musl" in _gcc:
-            cc = "gcc"
-        elif "musl" in _musl_gcc:
-            cc = "musl-gcc"
+        for compiler in ['gcc', 'musl-gcc']:
+            if self._test_compiler(compiler):
+                cc = compiler
+                break
         else:
-            assert False, "You need a working musl gcc (gcc or musl-gcc binary in PATH) to run C tests"
+            assert False, "Could not find working musl-gcc in PATH"
 
-        tester.f_makefile.write(f"{self.slug}: ../tests/{self.slug}.c\n\t{cc} $^ -o $@{ccflags}\n\n")
-        tester.make_targets.append(self.slug)
+        result = subprocess.run(
+            [cc, '-fdiagnostics-color=always', self._source, '-o', self._binary] + ccflags,
+            capture_output=True,
+            check=False,
+            text=True
+        )
 
-        super().prepare(tester)
+        error.put(f'Compiler {result.args} exited with code {result.returncode}\n')
+        error.put(result.stderr)
+        assert not result.stderr
+
+    def needs_build(self):
+        try:
+            return os.path.getmtime(self._source) >= os.path.getmtime(self._binary)
+        except FileNotFoundError:
+            return True
 
     def bind(self, box: sunwalker_box.Box):
         if self.files_committed:
@@ -108,7 +128,7 @@ class CTest(Test):
         path = f"/space/{self.slug}"
         self.argv = [path]
         box.touch(path)
-        box.bind(os.path.abspath(f"build/{self.slug}"), path, readonly=True)
+        box.bind(os.path.abspath(self._binary), path, readonly=True)
 
     @staticmethod
     def extract_header(path):
@@ -140,12 +160,34 @@ class YamlTest(Test):
 
 
 @dataclasses.dataclass
-class SingleTest:
+class Builder:
+    slugslug: str
+    slug: str
+    description: str
+    test: Test
+
+    def run(self, _core, error: queue.Queue):
+        self.test.build(error)
+
+
+@dataclasses.dataclass
+class Preparer:
+    slugslug: str
+    slug: str
+    description: str
+    test: Test
+
+    def run(self, _core, error: queue.Queue):
+        self.test.prepare()
+
+
+@dataclasses.dataclass
+class Invoker:
+    slugslug: str
     slug: str
     description: str
     test: Test
     script: str
-    arch: Optional[list[str]] = None
     outer_env: dict[str, str] = dataclasses.field(default_factory=lambda: {})
     quotas: dict[str, ...] = dataclasses.field(default_factory=lambda: {})
     slow: bool = False
@@ -159,7 +201,7 @@ class SingleTest:
     def prepare(self, tester):
         self.test.prepare(tester)
 
-    def run(self, box_config, core: int, error: queue.Queue):
+    def run(self, core: int, error: queue.Queue, box_config: sunwalker_box.BoxConfig):
         for key, value in self.outer_env.items():
             os.environ[key] = value
 
@@ -227,88 +269,13 @@ class SingleTest:
 
 
 @dataclasses.dataclass
-class TestStarted:
-    slug: str
-
-
-@dataclasses.dataclass
-class TestFinished:
-    slug: str
-    description: str
-    duration: float
-    ex: Exception
-    trace: str
-    error: queue.Queue
-
-    def __post_init__(self):
-        if not self.ex:
-            self.kind = "pass"
-        elif isinstance(self.ex, AssertionError):
-            self.kind = "failure"
-        else:
-            self.kind = "crash"
-
-    def as_strings(self):
-        time_text = f"{self.duration:.1f}s" if self.duration > 0.1 else "    "
-        yield f"\x1b[36m{time_text}\x1b[0m "
-
-        status = "\x1b[32m OK \x1b[0m" if self.kind == "pass" else "\x1b[91mFAIL\x1b[0m"
-        yield f'{status} {self.slug}'
-
-        if self.kind != "pass":
-            yield '\n'
-            yield f"\x1b[36m  {self.description}\x1b[0m\n"
-
-            while not self.error.empty():
-                message = self.error.get_nowait()
-                yield from ('  --- executor message ---\n  ', message.replace('\n', '\n  '), '\n')
-
-            if self.kind == "failure":
-                yield from ("\x1b[33m  ", str(self.ex).replace("\n", "\n  "), "\x1b[0m\n    ", '\n    '.join(self.ex.__notes__))
-            elif self.kind == "crash":
-                yield from ("\x1b[95m", self.ex, "\x1b[0m\n", self.trace)
-
-
-class Testset:
-    def __init__(self, len):
-        self.current = set()
-        self.count = 0
-        self.len = len
-
-    def started(self, slug):
-        self.current.add(slug)
-
-    def finished(self, slug):
-        self.current.remove(slug)
-        self.count += 1
-
-    def is_running(self):
-        return self.count != self.len
-
-    def __str__(self):
-        return f"[{self.count}/{self.len}] Testing {', '.join(self.current)}..."
-
-
-@dataclasses.dataclass
 class Tester:
     box_config: sunwalker_box.BoxConfig
     arch: str
+    cpus: list[int]
     allow: Optional[set[str]] = None
     block: Optional[set[str]] = None
-    hide_passed: bool = False
-    bail_on_fail: bool = False
-    current_status: bool = False
-
-    def __post_init__(self):
-        os.makedirs("build", exist_ok=True)
-
-        self.f_makefile = open("build/Makefile", "w")
-        self.make_targets: list[str] = []
-        self.tests: list[SingleTest] = []
-        self.skips = 0
-
-        os.makedirs("build/roots", exist_ok=True)
-        os.makedirs("build/assets", exist_ok=True)
+    conductor_args: Optional[dict[str, ...]] = None
 
     def __enter__(self):
         return self
@@ -317,10 +284,12 @@ class Tester:
         pass
 
     def register(self):
+        self.builders: list[Builder] = []
+        self.preparers: list[Preparer] = []
+        self.tests: list[Invoker] = []
         for test_file in os.listdir("tests"):
             slug, ext = test_file.rsplit(".", 1)
             if slug in (self.block or set()) or slug not in (self.allow or slug):
-                self.skips += 1
                 continue
 
             test_class = dict(c=CTest, py=PyTest, yaml=YamlTest)[ext]
@@ -329,117 +298,39 @@ class Tester:
             header = yaml.unsafe_load(yaml_header)
 
             if self.arch not in (header.get("arch") or self.arch):
-                self.skips += 1
                 continue
 
-            # I AM NORMAL I AM PERFECTLY SANE
-            self.tests.append(SingleTest(
+            test = test_class(slug, **{key: header.get(key) for key in "static root assets".split()})
+
+            if test.needs_build():
+                self.builders.append(Builder(slug, f"Compile {slug}", test=test, description=f"Compile steps for {slug}"))
+
+            if test.preparable:
+                self.preparers.append(Preparer(slug, f"Prepare {slug}", test=test, description=f"Prepare environment for {slug}"))
+
+            self.tests.append(Invoker(
                 slug,
-                test=test_class(slug, **{key: header.get(key) for key in "static root assets".split()}),
-                **{key: header.get(key) for key in "description script arch outer_env quotas slow".split()}
+                f"Test {slug}",
+                test=test,
+                **{key: header.get(key) for key in "description script outer_env quotas slow".split()}
             ))
 
+    def build(self):
+        os.makedirs("build", exist_ok=True)
+
+        if not conductor.Conductor('builder', 'Compiling', 'Compiled', self.builders, **self.conductor_args).run(self.cpus):
+            raise SystemExit(1)
+
     def prepare(self):
-        for test in self.tests:
-            test.prepare(self)
+        os.makedirs("build/roots", exist_ok=True)
+        os.makedirs("build/assets", exist_ok=True)
 
-        self.f_makefile.write("all: " + " ".join(self.make_targets))
-        self.f_makefile.close()
+        if not conductor.Conductor('preparer', 'Preparing', 'Prepared', self.preparers, **self.conductor_args).run(self.cpus):
+            raise SystemExit(1)
 
-        subprocess.run(["make", "-C", "build", "all"], check=True)
-
-    def _run_single_test(self, box_config, test, cpus, feedback):
-        try:
-            feedback.put(TestStarted(test.slug))
-
-            thread_name = threading.current_thread().name
-            cpu_index = int(thread_name.rsplit('_', 1)[1])
-            cpu = cpus[cpu_index]
-
-            error = queue.Queue()
-
-            start_time = time.time()
-            try:
-                test.run(box_config, cpu, error)
-            except Exception as e:
-                ex, trace = e, traceback.format_exc()
-            else:
-                ex, trace = None, None
-            end_time = time.time()
-
-            duration = end_time - start_time
-
-        except BaseException as e:
-            ex, trace, error, duraiton = e, traceback.format_exc(), queue.Queue(), locals().get('duration', 0)
-
-        feedback.put(TestFinished(test.slug, test.description, duration, ex, trace, error))
-
-    def run(self, cpus):
-        passes = 0
-        failures = 0
-        crashes = 0
-
-        feedback = queue.Queue()
-
-        executor = ThreadPoolExecutor(max_workers=len(cpus), thread_name_prefix='invoker')
-        for test in sorted(self.tests, key=lambda test: not test.slow):
-            executor.submit(self._run_single_test, self.box_config, test, cpus, feedback)
-
-        current = Testset(len(self.tests))
-        while current.is_running():
-            if self.current_status == 'show':
-                print(f'\r\x1b[K{current}', flush=True, end='')
-
-            try:
-                event = feedback.get()
-            except KeyboardInterrupt:
-                print()
-                break
-
-            if type(event) is TestStarted:
-                current.started(event.slug)
-
-            elif type(event) is TestFinished:
-                current.finished(event.slug)
-
-                if event.kind == "pass":
-                    passes += 1
-                elif event.kind == "failure":
-                    failures += 1
-                elif event.kind == "crash":
-                    crashes += 1
-
-                if not self.hide_passed or event.kind != 'pass':
-                    print(f'\r\x1b[K', *event.as_strings(), sep='', flush=True)
-                    if self.bail_on_fail:
-                        break
-
-            else:
-                crashes += 1
-                print(f'\r\x1b[K{event}', flush=True)
-                if self.bail_on_fail:
-                    break
-
-        executor.shutdown(wait=False, cancel_futures=True)
-        unknown = len(self.tests) - passes - failures - crashes
-
-        print('\r\x1b[K', end='')
-        print("  ".join(
-            pattern
-            .replace("{}", str(cnt))
-            .replace("{s}", "" if cnt == 1 else "s")
-            for pattern, cnt in
-            [
-                ("\x1b[32m{} test{s} passed\x1b[0m", passes),
-                ("\x1b[93m{} test{s} skipped\x1b[0m", self.skips),
-                ("\x1b[91m{} test{s} failed\x1b[0m", failures),
-                ("\x1b[95m{} test{s} crashed\x1b[0m", crashes),
-                ("\x1b[36m{} not tested\x1b[0m", unknown),
-            ]
-            if cnt > 0
-        ))
-
-        if failures > 0 or crashes > 0:
+    def run(self):
+        args = self.conductor_args | dict(intermediate_status=True)
+        if not conductor.Conductor('invoker', 'Testing', 'Tested', self.tests, **args).run(self.cpus, self.box_config):
             raise SystemExit(1)
 
 
@@ -453,6 +344,12 @@ def main():
     parser.add_argument('--logs', default='none', choices=['none', 'impossible', 'warn', 'notice'], help='sunwalker-box logging verbosity')
     parser.add_argument('--hide-passed', action='store_true', help='Hide status for passed tests')
     parser.add_argument('--bail-on-fail', action='store_true', help='Bail on first error')
+    parser.add_argument(
+        '--intermediate-status',
+        default='show',
+        choices=['show', 'hide'],
+        help='Controls whether intermediate statuses will persist'
+    )
     parser.add_argument(
         '--current-status',
         default='show' if os.isatty(1) else 'hide',
@@ -470,15 +367,21 @@ def main():
         with Tester(
             box_config=config,
             arch=args.arch,
+            cpus=args.cores,
             allow=set(args.allow),
             block=set(args.block),
-            hide_passed=args.hide_passed,
-            bail_on_fail=args.bail_on_fail,
-            current_status=args.current_status,
+
+            conductor_args=dict(
+                hide_passed=args.hide_passed,
+                bail_on_fail=args.bail_on_fail,
+                intermediate_status=args.intermediate_status == 'show',
+                current_status=args.current_status == 'show',
+            ),
         ) as tester:
             tester.register()
+            tester.build()
             tester.prepare()
-            tester.run(args.cores)
+            tester.run()
 
 
 if __name__ == "__main__":
