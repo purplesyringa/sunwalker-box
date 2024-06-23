59c3b20aae09f8ce526d4e46cf8e5e08afe78c9c
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-06-23 17:42:18 +0300

    nix: Zero-dep update-output-hashes

--- generate/update-output-hashes.nix
+++ generate/update-output-hashes.nix
@@ -1,49 +1,24 @@
 let
-  pkgs = import ((import <nixpkgs> {}).fetchFromGitHub (
-    with (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked; {
-      inherit owner repo rev;
-      sha256 = narHash;
-    }
-  )) {};
-in
-  with pkgs; let
-    isGit = s: (builtins.substring 0 4 s) == "git+";
-    contains = needle: haystack: (builtins.length (lib.splitString needle haystack)) > 1;
-    stripPrefix = s: builtins.substring 4 (builtins.stringLength s) s;
-    splitBy = sep: idx: s: (builtins.elemAt (lib.splitString sep s) idx);
-    gitURL = s: splitBy "?" 0 (stripPrefix s);
-    gitBranch = s:
-      if (contains "?branch=" s)
-      then splitBy "#" 0 (splitBy "?branch=" 1 (stripPrefix s))
-      else if (contains "?tag=" s)
-      then splitBy "#" 0 (splitBy "?tag=" 1 (stripPrefix s))
-      else null;
-    gitRev = s: splitBy "#" 1 (stripPrefix s);
-    gitHash = s:
-      (builtins.fetchGit (
-        (maybeRef (gitBranch s))
-        // {
-          url = gitURL s;
-          rev = gitRev s;
-        }
-      ))
-      .narHash;
-    maybeRef = s:
-      if s != null
-      then {ref = s;}
-      else {};
-    packages =
-      builtins.filter (x: x ? source && isGit x.source)
-      (builtins.fromTOML (builtins.readFile ./Cargo.lock)).package;
+  isGit = s: (builtins.substring 0 4 s) == "git+";
+  packages =
+    builtins.filter (x: x ? source && isGit x.source)
+    (builtins.fromTOML (builtins.readFile ../Cargo.lock)).package;
+  gitHash = source: let
+    matched = builtins.elemAt (builtins.match ''git\+([^?]*)(\?(branch|tag)=([^#]*))?#(.*)'' source);
   in
-    builtins.foldl' (
-      f: x:
-        f
-        // {
-          "${x.name + "-" + x.version}" = {
-            url = x.source;
-            narHash = gitHash x.source;
-          };
-        }
-    ) {}
-    packages
+    (builtins.fetchGit {
+      url = matched 0;
+      rev = matched 4;
+    })
+    .narHash;
+in
+  builtins.listToAttrs
+  (map
+    (package: {
+      name = "${package.name + "-" + package.version}";
+      value = {
+        url = package.source;
+        narHash = gitHash package.source;
+      };
+    })
+    packages)
